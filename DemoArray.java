59ee7fbfb3822b836db4b95803e23a195f6f369a
Timius <timius@bk.ru>
2016-12-30 20:34:26 +0800

    regenereition

--- DemoArray.java
+++ DemoArray.java
@@ -7,80 +7,82 @@ import java.util.Random;
  * Created by User on 23.11.2016.
  */
  public class DemoArray<T extends Number> {
-    T[] array;
-   private int length = 0;
 
-    DemoArray(T[] o) {
-        array = o;
+    protected Number[] array = new Number[10];
+
+    protected void creat(Number[] newArray) {
+        array = newArray;
     }
 
-    protected int max(){
-        int max = array[0].intValue();
-    for (int i = 0; i < array.length; i++)
-        if (max < array[i].intValue()) {
-            max = array[i].intValue();
+    protected void print() {
+        System.out.println(Arrays.toString(array));
+    }
+
+    public void addElem(T value) {
+        Number[] arrayN = new Number[array.length + 1];
+
+        for (int i = 0; i < array.length; i++) {
+            arrayN[i] = array[i];
         }
-//    System.out.println("max=" + max);
-    return max;
-}
-    protected int min(){
+        arrayN[array.length] = value;
+
+        array = arrayN;
+        System.out.println("Увеличенный массив" + Arrays.toString(array));
+    }
+
+    protected void deleteElement(int j) {
+        for (int i = j; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        Number[] arrayD = new Number[array.length - 1];
+        for (int i = 0; i < arrayD.length; i++) {
+            arrayD[i] = array[i];
+        }
+        array = arrayD;
+        System.out.println("Уменьшеный массив" + Arrays.toString(array));
+    }
+
+    protected int max() {
+        int max = (int) array[0];
+        for (int i = 0; i < array.length; i++)
+            if (max < (int) array[i]) {
+                max = (int) array[i];
+            }
+        System.out.println("max=" + max);
+        return max;
+    }
+
+    protected int min() {
         int min = array[0].intValue();
         for (int i = 0; i < array.length; i++)
             if (min > array[i].intValue()) {
                 min = array[i].intValue();
             }
-//        System.out.println("min=" + min);
+        System.out.println("min=" + min);
         return min;
     }
-    protected double avg(){
-        double sum = 0.0;
+
+    protected Number avg() {
+        double avg = 0;
+        int n = 0;
         for (int i = 0; i < array.length; i++)
-            sum += array[i].doubleValue();
-
-        return sum/array.length;
-    }
-    public void add(T i) { //добавить в конец
-
-        array[array.length-1] = i;
+            if (array[i].intValue() != 0) {
+                avg += array[i].intValue();
+                n++;
             }
-
-
-
-
-//    protected void addElement (T value){
-//
-//        T [] arrayN = new T [array.length+1];
-//        for (int i = 0; i < array.length; i++){
-//            arrayN[i]=array[i];}
-//
-//
-//
-//        for (int i = 0; i < array.length; i++){
-//            arrayN[i]=array[i];}
-//        for (int i = arrayN.length; i > j; i--) {
-//            arrayN[i-1] = arrayN[i - 2];
-//        }
-//        arrayN[j]= value;
-//        System.out.println("Увеличенный массив " + Arrays.toString(arrayN));
-//    }
-//
-//    protected int [] deleteElement (int j) {
-//        for (int i = j; i < array.length-1; i++) {
-//            array[i] = array[i + 1];
-//        }
-//
-//        int[] arrayD = new int[array.length - 1];
-//        for (int i = 0; i < arrayD.length; i++) {
-//            arrayD[i] = array[i];
-//        }
-//        int [] array = arrayD; //сделал вариант с перезаписыванием исходного массива
-////        System.out.println("Уменьшенный массив " + Arrays.toString(arrayD));
-//        System.out.println("Уменьшенный массив " + Arrays.toString(array));
-//        return array;
-//
-//    }
-
-
-
+        avg = avg / n;
+        System.out.println("avg=" + avg + "; n=" + n);
+        return avg;
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
--- Main.java
+++ Main.java
@@ -6,15 +6,27 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Integer iarray [] = {2, 234, 45, 455, 667, 398, 22, 0};
-        DemoArray<Integer> iArray=new DemoArray<>(iarray);
-        System.out.println(Arrays.toString(iarray));
+        DemoArray iArray = new DemoArray();
+        Integer [] iarray = {2, 234, 45, 455, 667, 398, 22, 0};
 
-        System.out.println(iArray.avg());
-        System.out.println(iArray.max());
-        System.out.println(iArray.min());
-        iArray.add(4);
-        System.out.println(Arrays.toString(iarray));
+        iArray.creat(iarray);
+
+        iArray.print();
+
+        iArray.addElem(34);
+        iArray.addElem(15);
+
+        iArray.deleteElement(3);
+        iArray.deleteElement(22);
+
+        iArray.max();
+        iArray.min();
+        iArray.avg();
+
+//        String sarray [] = {"sd", "asdas", "asd", "asd"};
+//        DemoArray<String> stringDemoArray = new DemoArray<>(sarray);
+//        System.out.println(Arrays.toString(sarray));
+
 
 
 
